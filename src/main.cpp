8bffa73cf60779012c88d3724decb73e33973ddc
kjh <wogudwkd1233@gmail.com>
2023-10-23 14:34:57 +0900

    add basic structure

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 bool isNight = false;
 int delayCheckNight = 0;
 
+bool buzzProximity = false;
+bool buzzFire = false;
+bool buzzRain = false;
+
 void CheckNight() {
   if(delayCheckNight) {
     --delayCheckNight;
@@ -13,9 +17,52 @@ void CheckNight() {
   }
 }
 
+void CheckProximity() {
+  if(isNight) {
+    //buzzProximity = SOME_FUNCTION_TO_CHECK_SENSOR();
+  }
+}
+void CheckFire() {
+  //buzzFire = SOME_FUNCTION_TO_CHECK_SENSOR();
+}
+void CheckRain() {
+  //buzzRain = SOME_FUNCTION_TO_CHECK_SENSOR();
+}
+
+void DoBridge() {
+  if(isNight) {
+    //SOME_FUNCTION_TO_CONTROL_MOTOR();
+  }
+}
+
+void DoBuzz() {
+  if(buzzRain) {
+    //SOME_FUNCTION_TO_CONTROL_BUZZER();
+  }
+  else if(buzzFire) {
+    //SOME_FUNCTION_TO_CONTROL_BUZZER();
+  }
+  else if(buzzProximity) {
+    //SOME_FUNCTION_TO_CONTROL_BUZZER();
+  }
+}
+
+void DoLight() {
+  if(isNight) {
+    //SOME_FUNCTION_TO_CONTROL_LED();
+  }
+}
+
 void setup() {
 }
 
 void loop() {
+  CheckNight();
+  CheckProximity();
+  CheckFire();
+  CheckRain();
 
+  DoBridge();
+  DoBuzz();
+  DoLight();
 }
